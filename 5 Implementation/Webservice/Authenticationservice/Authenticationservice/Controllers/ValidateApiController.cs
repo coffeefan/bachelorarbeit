035e726f2c7a609ce06933c9b4318e1f0dde8257
Christian Bachmann <Christian Bachmann>
2016-04-24 22:17:37 +0200

    Validation

---
+++ 5 Implementation/Webservice/Authenticationservice/Authenticationservice/Controllers/ValidateApiController.cs
@@ -0,0 +1,25 @@
+﻿using Authenticationservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Authenticationservice.Controllers
+{
+    public class ValidateApiController : ApiController
+    {
+        private IProjectAuthenticationRepository par;
+        public ValidateApiController() : this(new EF_ProjectAuthenticationRepository()) { }
+        public ValidateApiController(IProjectAuthenticationRepository temppar)
+        {
+            par = temppar;
+        }
+        [Route("api/Validate/")]
+        public AuthenticationStatus getValidate(int projectId, string providerId)
+        {
+            return par.OpenProjectAuthentication(projectId, providerId);
+        }
+    }
+}
---
+++ 5 Implementation/Webservice/Authenticationservice/Authenticationservice/Controllers/ValidateController.cs
@@ -0,0 +1,125 @@
+﻿using Authenticationservice.Models;
+using SecurityStepContract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authenticationservice.Controllers
+{
+    [Export("Validate", typeof(IController))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class ValidateController : Controller
+    {
+        private IProjectAuthenticationRepository par;
+
+        public ValidateController() : this(new EF_ProjectAuthenticationRepository()) { }
+        public ValidateController(IProjectAuthenticationRepository temppar)
+        {
+            par = temppar;
+        }
+
+        
+        public ActionResult Index() { 
+            return View();
+        }
+
+        public ActionResult Check(int projectId=-1, string providerId="", string sign="")
+        {
+            try
+            {
+                //Session Handling Input/Output
+                if (projectId != -1)
+                {
+                    System.Web.HttpContext.Current.Session["ProjectId"] = projectId;
+                    System.Web.HttpContext.Current.Session["ProviderId"] = providerId;
+                    System.Web.HttpContext.Current.Session["Sign"] = sign;
+                }
+                else
+                {
+                    projectId= (int)System.Web.HttpContext.Current.Session["ProjectId"];
+                    providerId= (string)System.Web.HttpContext.Current.Session["ProviderId"];
+                    sign = (string)System.Web.HttpContext.Current.Session["Sign"];
+                }            
+           
+                if (new ProjectsController().CheckSignIn(projectId, providerId, sign))
+                {
+                    AuthenticationStatus authenticationStatus=par.OpenProjectAuthentication(projectId, providerId);
+                    if (authenticationStatus == AuthenticationStatus.VALID)
+                    {
+                        return RedirectToAction("Finish");
+                    }
+                    
+
+                    List<ProjectSecurityStep> securitySteps =new EF_ProjectSecurityStepRepository().GetProjectSecurityStepList(projectId);
+
+                    foreach(ProjectSecurityStep projectSecurityStep in securitySteps)
+                    {
+                        ISecurityStepInfo securityStepInfo = new SecurityStepInfoContainer().getSecurityStepInfo(projectSecurityStep.SecurityStep+"Info");
+                        if (!securityStepInfo.checkIsValidated(projectId, providerId))
+                        {
+                            return RedirectToAction("Index", projectSecurityStep.SecurityStep);
+                        }
+                    }
+
+                    //Finish
+                    par.FinishProjectAuthentication(projectId, providerId);
+                    return RedirectToAction("Finish");
+
+
+                }
+                return RedirectToAction("Failedpage", new { errormessage = "Der SignIn Parameter konnte nicht bearbeitet werden" });
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Failedpage", new { errormessage = "Es ist ein aussergewöhnlicher Fehler aufgetreten" });
+            } 
+        }
+
+        public ActionResult Loading()
+        {
+            return View();
+        }
+
+        public ActionResult Failedpage(string errormessage)
+        {
+            @ViewBag.Title = "Fehler";
+            ViewBag.Message = errormessage;
+            return View();
+        }
+
+        
+
+        public ActionResult Finish()
+        {
+            int projectid = (int)System.Web.HttpContext.Current.Session["ProjectId"];
+            Project project=new ProjectsController().ProjectById(projectid);
+            string result=DownloadString(project.ReturnUrl + "?projectId=" +
+                (int)System.Web.HttpContext.Current.Session["ProjectId"] +
+                "&providerId=" + (string)System.Web.HttpContext.Current.Session["providerid"] +
+                 "&sign=" + (string)System.Web.HttpContext.Current.Session["sign"]);
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public string DownloadString(string address)
+        {
+            WebClient client = new WebClient();
+
+            string reply = client.DownloadString(address);
+
+            return reply;
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
